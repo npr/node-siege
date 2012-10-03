62257b1643d9ca9510e0946abba8907c270c0011
johnymonster <jnelson11@gmail.com>
2012-10-03 10:44:53 -0400

    Pushing out lots of changes for node-siege

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # node-siege
 
-Node wrapper for siege load testing
+Simple Node wrapper for siege load testing
 
 ## Getting Started
 Install the module with: `npm install node-siege`
 
 ```javascript
-var node_siege = require('node-siege');
-node_siege.awesome(); // "awesome"
+var siege = require('siege');
+siege('-t5s -c 10 -f urls.txt');
 ```
 
 ## Documentation
@@ -15,6 +15,16 @@ _(Coming soon)_
 
 ## Examples
 _(Coming soon)_
+
+## Testing
+Install grunt
+```
+sudo npm install grunt -g
+```
+Then run
+```
+grunt mochasimple
+```
 
 ## Contributing
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt](https://github.com/cowboy/grunt).
--- grunt.js
+++ grunt.js
@@ -1,11 +1,25 @@
 module.exports = function(grunt) {
 
+
+
   // Project configuration.
   grunt.initConfig({
     pkg: '<json:package.json>',
     test: {
       files: ['test/**/*.js']
     },
+      simplemocha: {
+          all: {
+              src: 'test/**/*.js',
+              options: {
+                  globals: ['should'],
+                  timeout: 2000,
+                  ignoreLeaks: false,
+                  ui: 'bdd',
+                  reporter: 'tap'
+              }
+          }
+      },
     lint: {
       files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
     },
@@ -33,7 +47,9 @@ module.exports = function(grunt) {
     }
   });
 
+    grunt.loadNpmTasks('grunt-simple-mocha');
+
   // Default task.
-  grunt.registerTask('default', 'lint test');
+  grunt.registerTask('default', 'lint simplemocha');
 
 };
